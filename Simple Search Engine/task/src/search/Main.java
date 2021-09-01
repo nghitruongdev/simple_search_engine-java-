e730d9517012625a84437430dc8673bf0e4e0368
Nghi Truong <89492293+nghitv2410@users.noreply.github.com>
2021-09-01 14:32:45 +0700

    Stage 01 Completed

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,7 +1,20 @@
 package search;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        String words = scanner.nextLine();
+        String s = scanner.next();
+        String[] list = words.split("\\s+");
+
+        for(int i = 0; i< list.length;i++){
+            if(list[i].equals(s)){
+                System.out.println(i+1);
+                return;
+            }
+        }
+        System.out.println("Not found");
     }
 }
